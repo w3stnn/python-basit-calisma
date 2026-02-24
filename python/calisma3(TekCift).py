5fb4d9600acef6320720ae81f1547f491b3d2652
Batın TAŞKIN <batintaskin7@gmail.com>
2026-02-24 21:01:49 +0300

    Update calisma3(TekCift).py

--- python/calisma3(TekCift).py
+++ python/calisma3(TekCift).py
@@ -2,5 +2,7 @@ sayi=int(input("bir sayi giriniz: "))
 
 if sayi%2==0:
     print(f"{sayi} cifttir...")
+elif sayi<0:
+    print("Girilen sayi sifirdan kucuk olamaz...")
 else:
     print(f"{sayi} tektir...")
